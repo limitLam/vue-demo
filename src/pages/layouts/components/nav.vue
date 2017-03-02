abf43eeefcd276fc616168390ba285af3d85360e
limit <minyi.lin@frontpay.cn>
2017-03-02 17:44:04 +0800

    update:nav

--- src/pages/layouts/components/nav.vue
+++ src/pages/layouts/components/nav.vue
@@ -8,7 +8,7 @@
           <el-menu-item index="todoList">todoList</el-menu-item>
         </el-menu-item-group>
         <el-menu-item-group title="分组2">
-          <el-menu-item index="1-3">选项3</el-menu-item>
+          <el-menu-item index="crement">crement</el-menu-item>
         </el-menu-item-group>
         <el-submenu index="1-4">
           <template slot="title">选项4</template>
